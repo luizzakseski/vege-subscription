ba9190370901531df2600e4e6d009e89222b8208
luizzakseski <l.zakseski@gmail.com>
2022-11-28 19:43:58 -0300

    added facebook login

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,6 +3,8 @@ export type AmplifyDependentResourcesAttributes = {
         "vegesubscriptionfaa1027e": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
